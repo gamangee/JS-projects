d820a84e8d3bbb88383b61330efc9e3c478a8052
gamangee <gamangee1991@gmail.com>
2023-01-16 16:22:50 +0900

    추가 기능 구현 : save localStorage

---
+++ todolist/src/context/DarkModeContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    updateDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    //Window.matchMedia()
+    //미디어쿼리 문자열의 분석 결과를 나타내는 객체를 반환하는 Web API
+    //prefers-color-scheme
+    //사용자의 시스템 색상 테마를 탐지하는 미디어 쿼리
+    const isDark =
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+}
+export const useDarkMode = () => useContext(DarkModeContext);
